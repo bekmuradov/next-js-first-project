0031058a78835e83deb034415fcd67eea7d8d401
Beck <47065940+bekmuradov@users.noreply.github.com>
2021-01-24 20:05:26 +0700

    added pokemon api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import Head from "next/head";
+import Link from "next/link";
+import {useRouter} from "next/router"
+
 function Home() {
-    return <div>Welcome, Explorer!</div>
+    const router = useRouter()
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push('/pokemon')
+    }
+  return (
+    <>
+      <Head>
+        <title>My Next JS Site</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>Welcome, Explorer!</div>
+      <Link href="/blog">
+        <a>Go to blog page</a>
+      </Link>
+      <button onClick={handleClick}>
+          Click me
+      </button>
+    </>
+  );
 }
 
-export default Home
+export default Home;
